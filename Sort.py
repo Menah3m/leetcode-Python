208a7fcaceca61b67a872b06be84c8208f3fc545
yasu <568200065@qq.com>
2020-03-31 22:06:01 +0900

    20200331 commit

---
+++ Sort.py
@@ -0,0 +1,14 @@
+
+'''QuickSort'''
+class Solution(object):
+    def sortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        le = len(nums)
+        for i in range(le-1):
+            for j in range(le-i-1):
+                if nums[j] > nums[j+1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+        return nums
