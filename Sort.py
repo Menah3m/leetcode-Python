cd128de1d689fd6543d836c03a26632784ef7045
yasu <568200065@qq.com>
2020-03-31 22:27:02 +0900

    20200331 commit

--- Sort.py
+++ Sort.py
@@ -2,12 +2,9 @@
 '''
 QuickSort
 时间复杂度:O(n*n)
-
-
 '''
-
 class Solution(object):
-    def sortArray(self, nums):
+    def quickSort(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
@@ -17,4 +14,24 @@ class Solution(object):
             for j in range(le-i-1):
                 if nums[j] > nums[j+1]:
                     nums[j], nums[j+1] = nums[j+1], nums[j]
+        return nums
+
+
+
+'''
+SelectSort
+时间复杂度：O(n*n)
+'''
+
+class Solution(object):
+    def selectSort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        le = len(nums)
+        for i in range(le):
+            for j in range(i, le):
+                if nums[j] < nums[i]:
+                    nums[j], nums[i] = nums[i], nums[j]
         return nums
