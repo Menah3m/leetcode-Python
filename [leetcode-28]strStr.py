0fef72410fa1e2497272cacb7f885f7ed42bc1a6
yasu <568200065@qq.com>
2020-05-07 21:45:57 +0900

    更新  实现strStr()函数

--- [leetcode-28]strStr.py
+++ [leetcode-28]strStr.py
@@ -1,3 +1,9 @@
+
+
+
+'''
+投机取巧法
+'''
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if needle == "" or haystack == needle: return 0
@@ -5,4 +11,17 @@ class Solution:
             return -1
         
         return haystack.find(needle) if needle in haystack else -1
-        
+
+'''
+暴力法
+'''
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "" or haystack == needle: return 0
+        if len(haystack) < len(needle): return -1
+
+        m = len(needle)
+        for i in range(len(haystack)-m+1):
+            if haystack[i:i+m] == needle:
+                return i
+        return -1
