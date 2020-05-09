f43b1619a709af917cf78e19cf32bfc3725cb252
yasu <568200065@qq.com>
2020-05-10 00:43:22 +0900

    更新 反转单链表

---
+++ [剑指offer-24]reverseLinkList.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+'''
+剑指offer-24题
+定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+'''
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next: return head
+
+        Last = None
+        
+        while head:
+            tmp = head.next
+            head.next = Last
+            Last = head
+            head = tmp
+
+        return Last
+
