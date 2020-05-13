82f8864ea2a9bc66e5cc746d1d26b368c1ec25d9
yasu <568200065@qq.com>
2020-05-13 19:16:21 +0900

    更新  买卖股票的最佳时机

---
+++ [leetcode-121]maxProfit.py
@@ -0,0 +1,23 @@
+'''
+leetcode-121
+
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
+注意：你不能在买入股票前卖出股票。
+'''
+
+'''
+一次遍历
+设置两个变量，分别储存截至目前最小的价格以及截至目前最大收益
+最后返回最大收益
+'''
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float("inf")
+        max_profit = 0
+
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price-min_price)
+
+        return max_profit
