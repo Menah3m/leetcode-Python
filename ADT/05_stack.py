0635061f7b0df361d60ccc7708661d82050155a4
yasu <568200065@qq.com>
2020-06-06 17:48:54 +0900

    更新 栈的ADT

---
+++ ADT/05_stack.py
@@ -0,0 +1,36 @@
+class Stack(object):
+    """栈"""
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        """添加元素到栈顶"""
+        self.stack.append(item)
+
+    def pop(self):
+        """弹出栈顶元素"""
+        self.stack.pop()
+
+    def peek(self):
+        """查看栈顶元素"""
+        return self.stack[-1]
+
+    def is_empty(self):
+        """判断是否为空"""
+        return self.stack == []
+
+    def size(self):
+        """返回栈的元素数量"""
+        return len(self.stack)
+
+
+s =Stack()
+
+s.push(2)
+print(s.peek())
+s.push(3)
+print(s.peek())
+print(s.size())
+s.pop()
+print(s.peek())
+print(s.is_empty())
