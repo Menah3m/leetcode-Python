d388e500bcc619f483010707760241a4986f344b
yasu <568200065@qq.com>
2020-06-06 18:02:03 +0900

    更新 单链表ADT

---
+++ ADT/02_single-linked-list.py
@@ -0,0 +1,38 @@
+class SingleNode(object):
+    """单链表"""
+    def __init__(self, val)：
+        self.val = val
+        self.next = None
+
+    def is_empty(self):
+        """链表是否为空"""
+        pass
+
+    def length(self):
+        """ 链表长度"""
+        pass
+
+    def travel(self):
+        """ 遍历整个链表"""
+        pass
+
+    def add(item):
+        """ 链表头部添加元素"""
+        pass
+
+    def append(item):
+        """ 链表尾部添加元素"""
+        pass
+
+    def insert(pos, item):
+        """ 指定位置添加元素"""
+        pass
+
+    def remove(item):
+        """ 删除节点"""
+        pass
+
+    def search(item): 
+        """查找节点是否存在"""
+        pass
+    
