d8cac1a6d9f297d0fd8c1ab4c482842ae73cc7eb
yasu <568200065@qq.com>
2020-06-17 21:34:22 +0900

    更新 合并两个有序链表

--- [leetcode-21]MergeTwoSortedLists.py
+++ [leetcode-21]MergeTwoSortedLists.py
@@ -33,3 +33,19 @@ class Solution:
                 ln2.next = recursive(ln2.next, ln1)
                 return ln2
         return recursive(l1, l2)
+
+
+# 递归
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l2.next, l1)
+            return l2
