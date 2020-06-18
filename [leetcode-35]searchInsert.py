35fe45ea4051b582260644e40ce466919dd6f37e
yasu <568200065@qq.com>
2020-06-18 23:24:10 +0900

    更新 查找插入位置

--- [leetcode-35]searchInsert.py
+++ [leetcode-35]searchInsert.py
@@ -20,3 +20,19 @@ class Solution:
             else:
                 left = mid + 1
         return res
+
+
+
+# 代码2
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                return mid
+        return l
