c178d9221a3f7565fc9fce989f0e42182af5d0ec
yasu <568200065@qq.com>
2020-07-04 22:19:35 +0900

    更新 删除排序链表中的重复元素2

--- [leetcode-82]remove-duplicates-from-sorted-linked-list-ii.py
+++ [leetcode-82]remove-duplicates-from-sorted-linked-list-ii.py
@@ -32,14 +32,14 @@ class Solution:
         pre = dummy_head
         while cur:
             cur_val = cur.val
-            is_dup = False     // flag作用:判断当前节点是否为重复值
-            while cur.next:    // 往下遍历，如果重复，则跳过该节点
+            is_dup = False     #flag作用:判断当前节点是否为重复值
+            while cur.next:    #往下遍历，如果重复，则跳过该节点
                 if cur.next.val == cur_val:
                     cur = cur.next
-                    is_dup = True    // flag置为1
+                    is_dup = True    #flag置为1
                 else:
-                    break         //若不重复，则结束该次遍历
-            if is_dup:            //若为重复元素，则删除节点
+                    break         #若不重复，则结束该次遍历
+            if is_dup:            #若为重复元素，则删除节点
                 pre.next = cur.next
             else:
                 pre = cur 
