dfa48bcef073a627990cf46950e356663dbd8eb3
yasu <568200065@qq.com>
2020-07-07 23:10:29 +0900

    更新 删除排序数组中的重复项

--- [leetcode-26]removeDuplicates.py
+++ [leetcode-26]removeDuplicates.py
@@ -29,9 +29,9 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         if not nums:return 0
 
-        i = 0
+        pos = 0
         for num in nums:
             if num != nums[i]:
-                i += 1
+                pos += 1
                 nums[i] = num
-        return i+1
+        return pos+1
