cff9e0afea7b3b34af272e3f54d3db25a8ae932e
yasu <568200065@qq.com>
2020-07-24 21:06:38 +0900

    更新 反转字符串

--- [leetcode-344]reverseString.py
+++ [leetcode-344]reverseString.py
@@ -16,14 +16,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-
-        front, end = 0, len(s)-1
-        
-        while front < end:
-            tmp = s[end]
-            s[end] = s[front]
-            s[front] = tmp
-            front += 1
-            end -= 1
-        
+        l, r = 0, len(s)-1
+        while l <= r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
         return s
