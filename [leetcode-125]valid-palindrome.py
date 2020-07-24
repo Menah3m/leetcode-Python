2e7a828a71ec05e9be1bd73773ee774e75eda707
yasu <568200065@qq.com>
2020-07-24 15:11:30 +0900

    更新  验证回文串

--- [leetcode-125]valid-palindrome.py
+++ [leetcode-125]valid-palindrome.py
@@ -32,6 +32,7 @@ class Solution:
             end -= 1
         return True
 
+#方法2
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
@@ -51,4 +52,22 @@ class Solution:
             else:
                 l += 1
                 r -= 1
+        return True
+
+# 方法3
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+
+        while l<r:
+            if  not s[l].isalnum():
+                l += 1
+                continue
+            if  not s[r].isalnum():
+                r -= 1
+                continue
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
         return True
