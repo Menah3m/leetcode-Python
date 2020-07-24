2553f1fe472f3d73beac3650cadb274270b4b4b2
yasu <568200065@qq.com>
2020-07-24 15:46:52 +0900

    更新 盛最多水的容器

--- [leetcode-11]containerWithMostWater.py
+++ [leetcode-11]containerWithMostWater.py
@@ -23,3 +23,18 @@ class Solution:
             else:
                 r -= 1
         return res
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0
+        temp_area = 0
+        l, r = 0, len(height)-1
+        while l < r:
+            temp_area = (r-l)*min(height[l],height[r])
+            max_area = max(temp_area, max_area)
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return max_area
+            
