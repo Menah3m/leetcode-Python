41fee4f72f64411fde2cbd63ab028e598b5fce35
yasu <568200065@qq.com>
2020-07-30 23:25:43 +0900

    更新 树的子结构

---
+++ [剑指offer-26]isSubStructure.py
@@ -0,0 +1,58 @@
+"""
+输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+
+B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+
+例如:
+给定的树 A:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+给定的树 B：
+
+   4 
+  /
+ 1
+返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
+
+示例 1：
+
+输入：A = [1,2,3], B = [3,1]
+输出：false
+示例 2：
+
+输入：A = [3,4,5,1,2], B = [4,1]
+输出：true
+
+来源：剑指offer-26
+链接：https://leetcode-cn.com/problems/shu-de-zi-jie-gou-lcof
+
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSubStructure(self, A: TreeNode, B: TreeNode) -> bool:
+        if not A or not B:
+            return False
+        return self.check(A, B) or self.isSubStructure(A.left, B) or self.isSubStructure(A.right, B)
+    
+    # s->ATree   t-> BTree
+    def check(self, s, t):
+        if not t:
+            return True
+        if not s:
+            return False
+        return s.val==t.val and self.check(s.left, t.left) and self.check(s.right, t.right)
+
+
+        
