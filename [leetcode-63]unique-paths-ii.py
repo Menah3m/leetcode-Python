6fe13cd38023ebb598a24aa4a635c6aed5478a5a
yasu <568200065@qq.com>
2020-08-06 23:56:11 +0900

    更新 不同路径2

---
+++ [leetcode-63]unique-paths-ii.py
@@ -0,0 +1,36 @@
+"""
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+
+现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+
+
+
+网格中的障碍物和空位置分别用 1 和 0 来表示。
+
+说明：m 和 n 的值均不超过 100。
+
+来源：LeetCode-63
+链接：https://leetcode-cn.com/problems/unique-paths-ii
+
+
+"""
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        height, width = len(obstacleGrid),len(obstacleGrid[0])
+        store = [[0]*width for i in range(height)]
+
+        #从上到下，从左到右
+        for m in range(height):#每一行
+            for n in range(width):#每一列
+                if not obstacleGrid[m][n]: #如果这一格没有障碍物
+                    if m == n == 0: #或if not(m or n)
+                        store[m][n] = 1
+                    else:
+                        a = store[m-1][n] if m!=0 else 0 #上方格子
+                        b = store[m][n-1] if n!=0 else 0 #左方格子
+                        store[m][n] = a+b
+        return store[-1][-1]
+
