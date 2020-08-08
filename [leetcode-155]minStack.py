0873d3461ae0579ee66dd575742285510e631ebd
yasu <568200065@qq.com>
2020-08-08 23:49:36 +0900

    更新  最小栈

--- [leetcode-155]minStack.py
+++ [leetcode-155]minStack.py
@@ -34,6 +34,39 @@ class MinStack:
         return self.min_stack[-1]
 
 
+#代码2
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min_stack = []
+
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if len(self.min_stack) == 0 or x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+
+
+    def pop(self) -> None:
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+        self.stack.pop()
+        
+
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+
+    def min(self) -> int:
+        return self.min_stack[-1]
+
+
+
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(x)
